d820a920cb8ddb51fe3369b0c027a76981979b10
DarioCeballoss <dario_tec_aut@hotmail.com.ar>
2023-09-28 19:33:56 -0300

    bd

--- Presentacion/FrmVerMat.cs
+++ Presentacion/FrmVerMat.cs
@@ -6,14 +6,25 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using Datos;
 
 namespace Presentacion
 {
     public partial class FrmVerMat : Form
     {
+        Conexion claseConexion = new Conexion();
+        DataTable Tabla = new DataTable();
+
         public FrmVerMat()
         {
             InitializeComponent();
+        }
+
+        private void FrmVerMat_Load(object sender, EventArgs e)
+        {
+            Tabla.Clear();
+            Tabla.Load(claseConexion.Leer("SELECT * FROM Alumno"));
+            dgvMatriculas.DataSource = Tabla;
         }
 
 
