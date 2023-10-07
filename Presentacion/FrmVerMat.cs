dfb2af8bb8f1fc3f199a0e992b1efff8efe876b2
DarioCeballoss <dario_tec_aut@hotmail.com.ar>
2023-10-07 16:43:08 -0300

    BD con datos agregados

--- Presentacion/FrmVerMat.cs
+++ Presentacion/FrmVerMat.cs
@@ -23,7 +23,7 @@ namespace Presentacion
         private void FrmVerMat_Load(object sender, EventArgs e)
         {
             Tabla.Clear();
-            Tabla.Load(claseConexion.Leer("SELECT * FROM Alumno"));
+            Tabla.Load(claseConexion.Leer("SELECT Alumno.Alumno_Nombres, Alumno.Alumno_Apellidos, Alumno.Alumno_Nacimiento, Alumno.Alumno_Dni, Tutor.Tutor_Nombres, Tutor.Tutor_Apellidos, Tutor.Tutor_Nacionalidad, Caracterizacion.Caracterizacion_Nombre FROM Tutor INNER JOIN (Caracterizacion INNER JOIN Alumno ON Caracterizacion.[Id] = Alumno.[Alumno_Caracterizacion]) ON Tutor.[Id] = Alumno.[Alumno_Tutor];"));
             dgvMatriculas.DataSource = Tabla;
         }
 
